2471070a849f309255d102f9686529a4c777af30
Remallya-16 <kremallya9@gmail>
2025-12-04 16:59:56 +0530

    save

---
+++ business-summary.py
@@ -0,0 +1,21 @@
+import sys
+
+if len(sys.argv) == 5:
+    shopname = sys.argv[1]
+    ownername = sys.argv[2]
+    GST = float(sys.argv[3])
+    totalsales = float(sys.argv[4])
+
+else:
+    print("No inputs given... using default values")
+
+    shopname = "Kiran Shop"
+    ownername = "Kishore"
+    GST = 5.0
+    totalsales = 10000
+
+    print("\n--- Business Summary (Default) ---")
+    print("Shop Name     :", shopname)
+    print("Owner Name    :", ownername)
+    print("GST (%)       :", GST)
+    print("Total Sales   :", totalsales)
